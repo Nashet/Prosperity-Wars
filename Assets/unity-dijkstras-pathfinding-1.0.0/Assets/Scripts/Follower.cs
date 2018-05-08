310fd8421303323613c89700d3c2d55d43178512
Nashet <Nashet@users.noreply.github.com>
2018-05-08 16:14:40 +0300

    style

--- Assets/unity-dijkstras-pathfinding-1.0.0/Assets/Scripts/Follower.cs
+++ Assets/unity-dijkstras-pathfinding-1.0.0/Assets/Scripts/Follower.cs
@@ -9,67 +9,67 @@ using UnityEngine;
 public class Follower : MonoBehaviour
 {
 
-	[SerializeField]
-	protected Graph m_Graph;
-	[SerializeField]
-	protected Node m_Start;
-	[SerializeField]
-	protected Node m_End;
-	[SerializeField]
-	protected float m_Speed = 0.01f;
-	protected Path m_Path = new Path ();
-	protected Node m_Current;
+    [SerializeField]
+    protected Graph m_Graph;
+    [SerializeField]
+    protected Node m_Start;
+    [SerializeField]
+    protected Node m_End;
+    [SerializeField]
+    protected float m_Speed = 0.01f;
+    protected Path m_Path = new Path();
+    protected Node m_Current;
 
-	void Start ()
-	{
-		m_Path = m_Graph.GetShortestPath ( m_Start, m_End );
-		Follow ( m_Path );
-	}
+    void Start()
+    {
+        m_Path = m_Graph.GetShortestPath(m_Start, m_End);
+        Follow(m_Path);
+    }
 
-	/// <summary>
-	/// Follow the specified path.
-	/// </summary>
-	/// <param name="path">Path.</param>
-	public void Follow ( Path path )
-	{
-		StopCoroutine ( "FollowPath" );
-		m_Path = path;
-		transform.position = m_Path.nodes [ 0 ].transform.position;
-		StartCoroutine ( "FollowPath" );
-	}
+    /// <summary>
+    /// Follow the specified path.
+    /// </summary>
+    /// <param name="path">Path.</param>
+    public void Follow(Path path)
+    {
+        StopCoroutine("FollowPath");
+        m_Path = path;
+        transform.position = m_Path.nodes[0].transform.position;
+        StartCoroutine("FollowPath");
+    }
 
-	/// <summary>
-	/// Following the path.
-	/// </summary>
-	/// <returns>The path.</returns>
-	IEnumerator FollowPath ()
-	{
-		#if UNITY_EDITOR
-		UnityEditor.EditorApplication.update += Update;
-		#endif
-		var e = m_Path.nodes.GetEnumerator ();
-		while ( e.MoveNext () )
-		{
-			m_Current = e.Current;
-			
-			// Wait until we reach the current target node and then go to next node
-			yield return new WaitUntil ( () =>
-			{
-				return transform.position == m_Current.transform.position;
-			} );
-		}
-		m_Current = null;
-		#if UNITY_EDITOR
-		UnityEditor.EditorApplication.update -= Update;
-		#endif
-	}
+    /// <summary>
+    /// Following the path.
+    /// </summary>
+    /// <returns>The path.</returns>
+    IEnumerator FollowPath()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.update += Update;
+#endif
+        var e = m_Path.nodes.GetEnumerator();
+        while (e.MoveNext())
+        {
+            m_Current = e.Current;
 
-	void Update ()
-	{
-		if ( m_Current != null )
-		{
-			transform.position = Vector3.MoveTowards ( transform.position, m_Current.transform.position, m_Speed );
-		}
-	}
-	
+            // Wait until we reach the current target node and then go to next node
+            yield return new WaitUntil(() =>
+          {
+              return transform.position == m_Current.transform.position;
+          });
+        }
+        m_Current = null;
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.update -= Update;
+#endif
+    }
+
+    void Update()
+    {
+        if (m_Current != null)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, m_Current.transform.position, m_Speed);
+        }
+    }
+
 }
--- Assets/unity-dijkstras-pathfinding-1.0.0/Assets/Scripts/Path.cs
+++ Assets/unity-dijkstras-pathfinding-1.0.0/Assets/Scripts/Path.cs
@@ -10,66 +10,66 @@ using UnityEngine;
 public class Path
 {
 
-	/// <summary>
-	/// The nodes.
-	/// </summary>
-	protected List<Node> m_Nodes = new List<Node> ();
-	
-	/// <summary>
-	/// The length of the path.
-	/// </summary>
-	protected float m_Length = 0f;
+    /// <summary>
+    /// The nodes.
+    /// </summary>
+    protected List<Node> m_Nodes = new List<Node>();
 
-	/// <summary>
-	/// Gets the nodes.
-	/// </summary>
-	/// <value>The nodes.</value>
-	public virtual List<Node> nodes
-	{
-		get
-		{
-			return m_Nodes;
-		}
-	}
+    /// <summary>
+    /// The length of the path.
+    /// </summary>
+    protected float m_Length = 0f;
 
-	/// <summary>
-	/// Gets the length of the path.
-	/// </summary>
-	/// <value>The length.</value>
-	public virtual float length
-	{
-		get
-		{
-			return m_Length;
-		}
-	}
+    /// <summary>
+    /// Gets the nodes.
+    /// </summary>
+    /// <value>The nodes.</value>
+    public virtual List<Node> nodes
+    {
+        get
+        {
+            return m_Nodes;
+        }
+    }
 
-	/// <summary>
-	/// Bake the path.
-	/// Making the path ready for usage, Such as calculating the length.
-	/// </summary>
-	public virtual void Bake ()
-	{
-		List<Node> calculated = new List<Node> ();
-		m_Length = 0f;
-		for ( int i = 0; i < m_Nodes.Count; i++ )
-		{
-			Node node = m_Nodes [ i ];
-			for ( int j = 0; j < node.connections.Count; j++ )
-			{
-				Node connection = node.connections [ j ];
-				
-				// Don't calculate calculated nodes
-				if ( m_Nodes.Contains ( connection ) && !calculated.Contains ( connection ) )
-				{
-					
-					// Calculating the distance between a node and connection when they are both available in path nodes list
-					m_Length += Vector3.Distance ( node.transform.position, connection.transform.position );
-				}
-			}
-			calculated.Add ( node );
-		}
-	}
+    /// <summary>
+    /// Gets the length of the path.
+    /// </summary>
+    /// <value>The length.</value>
+    public virtual float length
+    {
+        get
+        {
+            return m_Length;
+        }
+    }
+
+    /// <summary>
+    /// Bake the path.
+    /// Making the path ready for usage, Such as calculating the length.
+    /// </summary>
+    public virtual void Bake()
+    {
+        List<Node> calculated = new List<Node>();
+        m_Length = 0f;
+        for (int i = 0; i < m_Nodes.Count; i++)
+        {
+            Node node = m_Nodes[i];
+            for (int j = 0; j < node.connections.Count; j++)
+            {
+                Node connection = node.connections[j];
+
+                // Don't calculate calculated nodes
+                if (m_Nodes.Contains(connection) && !calculated.Contains(connection))
+                {
+
+                    // Calculating the distance between a node and connection when they are both available in path nodes list
+                    m_Length += Vector3.Distance(node.transform.position, connection.transform.position);
+                }
+            }
+            calculated.Add(node);
+        }
+    }
 
     internal Vector3[] GetVector3Nodes()
     {
@@ -87,14 +87,14 @@ public class Path
     /// </summary>
     /// <returns>A string that represents the current object.</returns>
     /// <filterpriority>2</filterpriority>
-    public override string ToString ()
-	{
-		return string.Format (
-			"Nodes: {0}\nLength: {1}",
-			string.Join (
-				", ",
-				nodes.Select ( node => node.name ).ToArray () ),
-			length );
-	}
-	
+    public override string ToString()
+    {
+        return string.Format(
+            "Nodes: {0}\nLength: {1}",
+            string.Join(
+                ", ",
+                nodes.Select(node => node.name).ToArray()),
+            length);
+    }
+
 }
