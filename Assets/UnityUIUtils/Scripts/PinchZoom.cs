6c9ad0ab7834a4a704a54de7c1f89a8b4a67a5d8
Nashet <not@mmm.com>
2023-02-08 23:34:55 +0100

    Add pinch zoom

---
+++ Assets/UnityUIUtils/Scripts/PinchZoom.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Nashet.EconomicSimulation
+{
+    public class PinchZoom : MonoBehaviour
+    {
+        [SerializeField] private float zoomSpeed = 0.01f; 
+        private Vector2 prevDist = new Vector2(0, 0);
+        private Vector2 curDist = new Vector2(0, 0);
+
+        void Update()
+        {
+            CheckForMultiTouch();
+        }
+
+        private void CheckForMultiTouch()
+        {
+            // These lines of code will take the distance between two touches and zoom in - zoom out at middle point between them
+            if (Input.touchCount == 2 && Input.GetTouch(0).phase == TouchPhase.Moved && Input.GetTouch(1).phase == TouchPhase.Moved)
+            {                
+                curDist = Input.GetTouch(0).position - Input.GetTouch(1).position; //current distance between finger touches
+                prevDist = ((Input.GetTouch(0).position - Input.GetTouch(0).deltaPosition) - (Input.GetTouch(1).position - Input.GetTouch(1).deltaPosition)); //difference in previous locations using delta positions
+                float touchDelta = curDist.magnitude - prevDist.magnitude;
+
+                // Zoom out
+                if (touchDelta > 0)
+                {
+                    MainCamera.Get.Move(0f, zoomSpeed * -1f, 0f);
+                }
+
+                //Zoom in
+                else if (touchDelta < 0)
+                {
+                    MainCamera.Get.Move(0f, zoomSpeed, 0f);
+                }
+            }
+        }
+    }
+}
