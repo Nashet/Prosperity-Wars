16c8f4312e964e748e1930cdf4b96a5cbf6f9344
Nashet <not@mmm.com>
2023-07-27 20:58:52 +0200

    Add rivers to example

---
+++ Assets/Map/Examples/Country.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Nashet.Map.Examples
+{
+	public class Country
+	{
+		public Color NationalColor { get; protected set; }
+		private Material borderMaterial;
+		private string name;
+
+		public Country(Color nationalColor, string name)
+		{
+			NationalColor = nationalColor;
+			this.name = name;
+		}
+	}
+}
---
+++ Assets/Map/Examples/Province.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nashet.Map.Utils;
+using Nashet.MapMeshes;
+using QPathFinder;
+using UnityEngine;
+
+namespace Nashet.Map.Examples
+{
+	public class Province : IProvince
+	{
+		public enum TerrainTypes { Plains, Mountains }
+
+		static public Dictionary<int, Province> AllProvinces = new Dictionary<int, Province>();
+
+		public object Country { set; get; }
+		public TerrainTypes Terrain { get; internal set; }
+
+		public readonly HashSet<Border> neughbors = new HashSet<Border>();
+		public Node Node;
+		public Vector3 Position;
+		public int Id;
+		public ProvinceMesh provinceMesh;
+		private string name;
+
+		public Province(int Id, string name)
+		{
+			AllProvinces.Add(Id, this);
+			Terrain = TerrainTypes.Plains;
+			Rand.Call(() => Terrain = TerrainTypes.Mountains, 10);
+			this.Id = Id;
+			this.name = name;
+		}
+
+		internal bool isNeighbor(Province province2)
+		{
+			return neughbors.Any(x => x.Province == province2 && x.IsPassable);
+		}
+
+		internal bool isRiverNeighbor(Province neighbor)
+		{
+			return neughbors.Any(x => x.Province == neighbor && x.IsRiverBorder);
+		}
+
+		internal void AddRiverBorder(Province province2, Material riverMaterial)
+		{
+			provinceMesh.SetBorderMaterial(province2.Id, riverMaterial);
+			var border = neughbors.FirstOrDefault(x => x.Province == province2);
+			border.IsRiverBorder = true;
+		}
+
+		public override string ToString()
+		{
+			return name;
+		}
+	}
+}
